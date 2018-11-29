6955077ba3ad0a82f9abe9418ee34888ba6127ff
Muhammad Iqbal Kharis Firismanda <ryuka.1111@gmail.com>
2018-11-29 22:22:45 +0700

    membuat kelas items serta menambahkan 	- Variabel 	- Konstruktor 	- Getter and Setter (default) 	- Get Total Biaya

--- source package/itemsClass.java
+++ source package/itemsClass.java
@@ -13,12 +13,43 @@ public class itemsClass {
     float   harga;          //variabel untuk menyimpan harga item yang terdaftar
     int     banyakItems;    //variabel untuk menyimpan banyak item yang dibeli
 
+    public itemsClass(){}
     public itemsClass(String nama, float harga, int banyakItems) { //membuat konstruktor
         this.nama = nama;
         this.harga = harga;
         this.banyakItems = banyakItems;
     }
 
+    public String getNama() { //membuat get untuk nama item
+        return nama;
+    }
 
+    public void setNama(String nama) { //membuat set untuk nama item
+        this.nama = nama;
+    }
+
+    public float getHarga() { //membuat get untuk harga item
+        return harga;
+    }
+
+    public void setHarga(float harga) { //membuat set untuk harga item
+        this.harga = harga;
+    }
+
+    public int getBanyakItems() { //membuat get untuk banyaknya item yang dipesan
+        return banyakItems;
+    }
+
+    public void setBanyakItems(int banyakItems) { //membuat set untuk banyaknya item yang dipesan
+        this.banyakItems = banyakItems;
+    }
+
+    public float getTotal(){ //membuat get untuk menghitung biaya total pemesanan item
+        return this.harga * this.banyakItems;
+    }
+    
+    public String toString(){
+        return this.nama;
+    }
     
 }
