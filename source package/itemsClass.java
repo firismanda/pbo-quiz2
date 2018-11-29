2be647694455f6c783a4a4cd4f97f4a91495de0a
Muhammad Iqbal Kharis Firismanda <ryuka.1111@gmail.com>
2018-11-29 22:20:51 +0700

    membuat kelas items serta menambahkan 	- Variabel

---
+++ source package/itemsClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asus ROG
+ */
+public class itemsClass {
+    String  nama;           //variabel untuk menyimpan nama item yang terdaftar 
+    float   harga;          //variabel untuk menyimpan harga item yang terdaftar
+    int     banyakItems;    //variabel untuk menyimpan banyak item yang dibeli
+
+
+}
