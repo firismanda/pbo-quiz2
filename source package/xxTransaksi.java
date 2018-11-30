f73fc436a2d21c9cb7fed58cc9d89b315ce3ddb6
Muhammad Iqbal Kharis Firismanda <ryuka.1111@gmail.com>
2018-11-30 15:52:54 +0700

    membuat kelas xxTransaksi.java serta membuat variabel

---
+++ source package/xxTransaksi.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asus ROG
+ */
+public class xxTransaksi {
+    private String code; //variabel untuk menyimpan Kode Transaksi
+    private ArrayList<itemsClass> items = new ArrayList<>(); //variabel untuk menyimpan itemsClass
+    private float total; // variabel untuk menyimpan total
+
+}
